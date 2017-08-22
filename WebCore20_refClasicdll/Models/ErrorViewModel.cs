afe96b77be20ddfe4ba49a5f525361742d3e482d
René Pacios <rene15009@hotmail.com>
2017-08-22 21:00:21 +0200

    Added mvc core project

---
+++ WebCore20_refClasicdll/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebCore20_refClasicdll.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
